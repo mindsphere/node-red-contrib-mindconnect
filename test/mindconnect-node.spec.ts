8425b9817d6b7bab5b7039c9eb308c2eb1af21d3
sn0wcat <sn0wcat@users.noreply.github.com>
2019-03-14 04:37:38 +0100

    chore: tests

--- test/mindconnect-node.spec.ts
+++ test/mindconnect-node.spec.ts
@@ -1,5 +1,6 @@
 // Copyright Siemens AG, 2019
 
+import { IMindConnectConfiguration } from "@mindconnect/mindconnect-nodejs";
 import * as chai from "chai";
 import mcnode = require("../src/mindconnect.js");
 import helper = require("node-red-node-test-helper");
@@ -19,15 +20,37 @@ describe("MindConnect Node-RED node", () => {
         helper.unload();
     });
 
+    const sharedSecretConfig: IMindConnectConfiguration = require("../agentconfig.json");
+
+    const nodeTemplate = {
+        type: "mindconnect",
+        configtype: "SHARED_SECRET",
+        agentconfig: JSON.stringify(sharedSecretConfig),
+        privatekey: "",
+        model: "",
+        validate: true,
+        validateevent: true,
+        chunk: false,
+        disablekeepalive: false,
+        retry: "3"
+    };
+
     it("should instantiate", done => {
         helper.should.not.be.null;
         helper.should.not.be.undefined;
         mcnode.should.not.be.null;
-        const flow = [{ id: "n1", type: "mindconnect", z: 320, name: "mindconnect", configtype: "RSA_3072" }];
+        const flow = [
+            {
+                id: "n1",
+                name: "mindconnect",
+                ...nodeTemplate
+            }
+        ];
 
         helper.load(mcnode, flow, () => {
             const n1 = helper.getNode("n1");
             n1.should.have.property("name", "mindconnect");
+            n1.should.have.property("configtype", "SHARED_SECRET");
             done();
         });
     });
